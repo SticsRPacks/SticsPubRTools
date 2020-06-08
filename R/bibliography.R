6edfff34e10a1d31b422ae343295c51cda860282
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2020-06-08 16:46:10 +0200

    generate_bibfile: * renamed to gen_bibtex_file * multiple query (while) moved to new function get_references and replaced by a call to this function * new overwrite arg. * file checks * stop if NULL BibTex object returned * returns an invisible BibTex object
    read_zotero_references : now uses only the livre_rouge group id
    
    get_query_params : added collection and item type args for query params
    
    added: functions for query mode definition

--- R/bibliography.R
+++ R/bibliography.R
@@ -1,11 +1,11 @@
 #' Generating a BibTex file using a query to a Zotero web base
 #'
-#' @param file A BibTex file path (with a .bib extension)
+#' @param file_path A BibTex file path (with a .bib extension)
 #' @param query A character string to use for querying bibliographic base
-#' @param query_mode A query mode: "full" (the default) for searching in all the fields,
+#' @param query_type A query type: "full" (the default) for searching in all the fields,
 #' or "basic" for searching only in title, creator, year fields
-#' @param start A numeric indicating from which record number in the base to start the query
-#' @param verbose A logical for displayin messages from the query process (TRUE) or not (FALSE)
+#' @param verbose A logical for displaying messages from the query process (TRUE) or not (FALSE)
+#' @param overwrite A logical for deleting existing file (TRUE), or not (FALSE, default)
 #'
 #@return
 #'
@@ -13,66 +13,138 @@
 #'
 #' @examples
 #' \dontrun{
-#' generate_bibfile("file.bib")
 #'
-#' generate_bibfile(file = "file.bib", query = "1995" )
+#' # No query
+#' gen_bibtex_file("file.bib")
 #'
-#' generate_bibfile(file = "file.bib", query = "1995", query_mode = "basic" )
+#' # with query, searching in all fields
+#' gen_bibtex_file(file = "file.bib", query = "1995" )
+#'
+#' # searching in basic fields (title, creator, year)
+#' gen_bibtex_file(file = "file.bib", query = "1995", query_type = "basic" )
+#'
+#' # getting returned BibEntry object
+#' bibentry_obj <- gen_bibtex_file("file.bib")
 #'
 #'}
 #'
 #'
-generate_bibfile <- function( file = NULL ,
-                              query = "",
-                              query_mode ="full",
-                              start = 1,
-                              verbose = FALSE) {
+gen_bibtex_file <- function( file_path = NULL ,
+                             query = "",
+                             query_type ="full",
+                             verbose = FALSE,
+                             overwrite = FALSE) {
 
-  # file: with a .bib extension
-  if (base::is.null(file)) stop("File path is missing !")
+  # Checking file + .bib extension
+  if (base::is.null(file_path)) stop("File path is missing !")
 
-  if (!grepl(pattern = "\\.bib$", x = file)) stop("The file must have a .bib extension !")
+  if (!grepl(pattern = "\\.bib$", x = file_path)) stop("The file must have a .bib extension !")
 
-  #print(start)
-  BibEntry_obj_full <- get_zotero_references( query = query, query_mode = query_mode, start = start)
+  # Checking file, against overwrite arg.
+  if ( file.exists(file_path) ) {
+    if (overwrite) {
+      unlink(x = file_path)
+    } else {
+      stop("Consider to use overwrite = TRUE, for overwriting the existing file !")
+    }
+  }
+
+
+  # Getting BibEntry of all references from the zotero base
+  BibEntry_obj <- get_references(query = query, query_type = query_type)
+
+  # Checking content, aborting if NULL
+  if(base::is.null(BibEntry_obj)) stop("Not any references returned by the query !")
+
+  # Writing the .bib file
+  RefManageR::WriteBib( bib = BibEntry_obj,
+                        file = file_path,
+                        biblatex = FALSE,
+                        verbose = verbose)
+
+  # Returning bib list as BibEntry object
+  return(invisible(BibEntry_obj))
+}
+
+
+get_references <- function(query = "", query_type = "basic", verbose = FALSE) {
+
+  # Fixing options for just warning
+  RefManageR::BibOptions(check.entries = "warn")
+
+  # First query (will contain the merged queries resutls in the end)
+  BibEntry_obj_full <- read_zotero_references( query = query, query_type = query_type)
+
+  # if no references found
+  if(base::is.null(BibEntry_obj_full)) return()
+
+  # Starting a multiple query and requests merging
   start <- length(BibEntry_obj_full) + 1
 
   while (TRUE) {
-    print(start)
 
-    BibEntry_obj <- get_zotero_references( query = query, query_mode = query_mode, start = start)
+    if(verbose) print(start)
 
+    BibEntry_obj <- read_zotero_references( query = query, query_type = query_type, start = start)
+
+    # Reference number got from the query
     n_ref <-  length(BibEntry_obj)
 
+    # No more references in the base from start id value
     if (!n_ref) break
 
-    # merging BibEntry objects
+    # Merging BibEntry objects
     BibEntry_obj_full <- merge(x = BibEntry_obj_full,
                                y = BibEntry_obj)
 
+    # Next start id for the query
     start <- start + n_ref
 
   }
 
-  RefManageR::WriteBib( bib = BibEntry_obj_full, file = file, biblatex = FALSE, verbose = verbose)
-
-  #return(BibEntry_obj_full)
+  # Returning a BibEntry object
+  return(BibEntry_obj_full)
 }
 
 
-get_zotero_references <- function(query = "", query_mode = "basic", start = 1) {
 
-  group_livre_rouge <- "2450934"
+# @param start A numeric indicating from which record number in the base to start the query
 
-  params <- get_query_params( query = query, query_mode = query_mode, start = start)
-  delete_file <- TRUE
-  file <- get_temp_bib_file()
+read_zotero_references <- function(query = "", query_type = "basic", start = 1) {
 
-  r <- RefManageR::ReadZotero(group = group_livre_rouge, .params = params,
-                              delete.file = delete_file,
-                              temp.file = file)
+  group_id <- "2450934" # livre_rouge
 
+  params <- get_query_params( query = query, query_type = query_type, start = start)
+
+  r <- RefManageR::ReadZotero(group = group_id,
+                              .params = params)
   return(r)
+}
+
+
+get_query_params <- function( key = NULL,
+                              query="",
+                              query_type = "basic",
+                              start = 1,
+                              collection = NULL,
+                              item_type = NULL) {
+
+  qmode <- get_query_mode(query_type = query_type)
+
+  if (base::is.null(qmode)) stop("Unknown query mode (basic, full)")
+
+  # Getting a default read access API key
+  if (base::is.null(key)) key <- get_access_key(access = "write")
+
+  # Base params list
+  params <- list( q = query, key = key, qmode = qmode , start = start)
+
+  # Specific params
+  if (!base::is.null(collection)) params$collection <- collection # collection name !
+
+  if (!base::is.null(item_type)) params$itemType <- item_type
+
+  return(params)
 }
 
 
@@ -88,33 +160,23 @@ get_access_key <- function(access = "read") {
 
 }
 
-get_collection_id <- function(coll_name = "livre_rouge_2020") {
 
-  if (coll_name == "livre_rouge_2020") coll_id <- "QG2JDQ95"
+get_query_mode <- function(query_type) {
 
-  return(coll_id)
+  qmodes <- get_query_modes()
+
+  if (!query_type %in% names(qmodes)) return()
+
+  return(qmodes[[query_type]])
+}
+
+get_query_modes <- function() {
+
+  qmodes <- list()
+  qmodes$basic <- "titleCreatorYear"
+  qmodes$full <- "everything"
+
+  return(qmodes)
 
 }
 
-get_query_params <- function( key = NULL,
-                              query="",
-                              query_mode = "basic",
-                              start = 1) {
-
-  qmode <- "titleCreatorYear"
-  if (query_mode == "full") qmode <- "everything"
-
-  # getting collection id
-  coll_id <- get_collection_id()
-
-  # getting a default read access API key
-  if (base::is.null(key)) key <- get_access_key(access = "write")
-
-  #list( limit = limit, q = query, key = key, qmode = qmode , collection = coll_id, start = start)
-  list( q = query, key = key, qmode = qmode , collection = coll_id, start = start)
-}
-
-
-get_temp_bib_file <- function() {
-  tempfile(fileext = ".bib")
-}
