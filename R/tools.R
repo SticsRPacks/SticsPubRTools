76fe8a301e55aa9b4119e9ce16fa9f4d44725e64
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2020-06-17 17:53:54 +0200

    added checking if string starting is a character

--- R/tools.R
+++ R/tools.R
@@ -1,6 +1,12 @@
-get_label_from_name <- function(name = NULL, sep = "-") {
+get_label_from_name <- function(name = NULL, sep = "-", no_number_first = TRUE) {
 
   if ( !base::is.character(name) ) return()
+
+  # Checking first character
+  if (no_number_first) {
+    is_char <- suppressWarnings(base::is.na(as.numeric(substr(name,1,1))))
+    if (!is_char) stop("Only letters as string starting character !")
+  }
 
   # name treatment
   # convert to lower case valid variable name
@@ -11,6 +17,9 @@ get_label_from_name <- function(name = NULL, sep = "-") {
 
   # Removing trailing and heading "-"
   label <- trimws(x = label, whitespace = sep)
+
+
+
 
   return(label)
 
