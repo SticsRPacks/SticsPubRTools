fcc4030579d147014bcf2c97421f2f325050438a
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2020-07-24 11:03:07 +0200

    warnings mgmt
    changes appearance + css use
    
    add comments and removed useless ones

--- R/find_stics_names.R
+++ R/find_stics_names.R
@@ -1,13 +1,21 @@
 find_stics_names <- function() {
+  # Masquing warnings display during app execution
+  options(warn=-1)
+  on.exit(options(warn=0))
 
-  css <- "
+  css <-
+  "
   #name {
-    color: red;
+    color: blue;
   }
   #table {
     background: #f2f2f2;
   }
-"
+  #insert {
+    background-color:#337ab7;
+    color:#e8f0f7;
+  }
+  "
   stics_versions <- SticsRFiles::get_stics_versions_compat()$versions_list
   last_version <- SticsRFiles::get_stics_versions_compat()$last_version
 
@@ -16,44 +24,39 @@ find_stics_names <- function() {
                          miniUI::gadgetTitleBar("Search a Stics parameter or variable"),
                          miniUI::miniContentPanel(
 
-                           fillRow(height = "60%",
-                                   fillCol(
-                                     shiny::selectInput(inputId = "type", label = shiny::strong("Type of name"),
-                                                        choices = c("variable", "parameter"),selected = "parameter")
-                                   ),
-                                   fillCol(
-                                     shiny::selectInput(inputId = "version", label = shiny::strong("Stics version"),
-                                                        choices = stics_versions,
-                                                        selected = last_version))
+                           shiny::fillRow(height = "40%",
+                                          shiny::fillCol(width = "70%",
+                                            shiny::selectInput(inputId = "type", label = shiny::strong("Type of name"),
+                                                               choices = c("variable", "parameter"),selected = "parameter")
+                                          ),
+                                          shiny::fillCol(width = "70%",
+                                            shiny::selectInput(inputId = "version", label = shiny::strong("Stics version"),
+                                                               choices = stics_versions,
+                                                               selected = last_version))
                            ),
 
-                           fillRow(height = "40%",
-                                   fillCol(shiny::textInput(inputId = "name", label = shiny::strong("Searched name") ,
-                                                            width = "80%" , placeholder = "Enter a name or a part of")
-                                   ),
-                                   fillCol(
-                                     shiny::checkboxInput(inputId = "format", label = "Format name for RMarkdown", value = TRUE)
-                                   )
+                           shiny::fillRow(height = "40%",
+                                          shiny::fillCol(
+                                            shiny::textInput(inputId = "name", label = shiny::strong("Searched name") ,
+                                                             width = "80%" , placeholder = "Enter a name or a part of")
+                                          ),
+                                          shiny::fillCol(
+                                            shiny::checkboxInput(inputId = "format", label = "Format name for RMarkdown", value = TRUE)
+                                          )
 
                            ),
+                           shiny::textOutput(outputId = "number")
                          ),
 
                          miniUI::miniButtonBlock(
                            shiny::actionButton("insert", "Insert name"),
-                           # shiny::actionButton("clean", "Clean output"),
                            border = "top"
                          ),
 
                          miniUI::miniContentPanel(
                            # miniUI::gadgetTitleBar("Results"),
                            shiny::tableOutput(outputId = "table")
-                         )#,
-
-                         # miniUI::miniButtonBlock(
-                         #   shiny::actionButton("insert", "Insert name"),
-                         #   # shiny::actionButton("clean", "Clean output"),
-                         #   border = "top"
-                         # )
+                         )
 
   )
 
@@ -64,9 +67,9 @@ find_stics_names <- function() {
     names_type <- shiny::reactive({substr(input$type, start = 1, stop = 3)})
 
     # Getting table an filtering it
-    # Displaying only the 2 1st columns
+    #
     names_table <- shiny::reactive({
-      print(input$version)
+      #print(input$version)
       if (length(in_name())) {
         l <- get_names_list(type = names_type(), stics_version = names_version())
         id <- grepl(pattern = make_pattern(in_name()), x = l[[1]])
@@ -77,12 +80,17 @@ find_stics_names <- function() {
     # getting format activation
     names_format <- shiny::reactive({input$format})
 
+    # getting selected version
     names_version <- shiny::reactive({input$version})
 
+    # getting the name typed
     in_name <- shiny::reactive({input$name})
 
+    # getting the found number of par or var
+    #names_number <- shiny::reactive({nrow(output$table)})
 
-    # Changing name
+
+    # Handle the name typed on table selection
     shiny::observeEvent(input$name, {
       output$table <- shiny::renderTable({
         names_table()
@@ -92,7 +100,7 @@ find_stics_names <- function() {
       )
     })
 
-    # Handle the type selection in dropdown list
+    # Handle the type selection in dropdown list on loaded table
     shiny::observeEvent(input$type, {
       output$table <- shiny::renderTable({
         names_table()
@@ -102,6 +110,7 @@ find_stics_names <- function() {
       )
     })
 
+    # Handle the version selection on table loaded for a given type
     shiny::observeEvent(input$version, {
       output$table <- shiny::renderTable({
         names_table()
@@ -109,6 +118,7 @@ find_stics_names <- function() {
       caption.placement = getOption("xtable.caption.placement", "top"),
       caption.width = getOption("xtable.caption.width", NULL)
       )
+      #output$number <- renderText(nrow(output$table))
     })
 
     # Handle the Insert button being pressed.
@@ -140,18 +150,10 @@ find_stics_names <- function() {
 
     })
 
-    # Quitting the app
+    # Handle the done button click for quitting the app
     shiny::observeEvent(input$done, {
       shiny::stopApp("Bye")
     })
-
-
-    # shiny::observeEvent(input$clean, {
-    #   output$table <- shiny::renderTable({
-    #   })
-    # })
-
-
 
   }
 
