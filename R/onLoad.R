809ff19c95e8ca23959c1eb9b1e179438a60102f
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2020-09-02 11:26:00 +0200

    updating automatically V10.0 files when loading the package, according to the repos version.

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,4 +1,10 @@
 .onLoad <- function(libname, pkgname...) {
   # Creating the stics environment
   #invisible(SticsRFiles::sticsenv())
+
+  # Updating files for the V10.0 version, inputs.csv and outputs.csv
+  suppressMessages(
+    SticsRFiles:::update_stics_version(
+      url = "https://w3.avignon.inra.fr/svn/modulostics/branches/modulo_stics_perennes" ,
+      version_name = "V10.0"))
 }
