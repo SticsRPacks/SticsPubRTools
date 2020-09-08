05e347f602a17806d8df5b38c23c2a3a2a5aaa54
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2020-09-08 18:15:01 +0200

    suppressed : updating versions infos with V10.0

--- R/onLoad.R
+++ R/onLoad.R
@@ -3,8 +3,8 @@
   #invisible(SticsRFiles::sticsenv())
 
   # Updating files for the V10.0 version, inputs.csv and outputs.csv
-  suppressMessages(
-    SticsRFiles:::update_stics_version(
-      url = "https://w3.avignon.inra.fr/svn/modulostics/branches/modulo_stics_perennes" ,
-      version_name = "V10.0"))
+  # suppressMessages(
+  #   SticsRFiles:::update_stics_version(
+  #     url = "https://w3.avignon.inra.fr/svn/modulostics/branches/modulo_stics_perennes" ,
+  #     version_name = "V10.0"))
 }
