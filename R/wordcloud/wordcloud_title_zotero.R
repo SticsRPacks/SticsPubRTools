01a9007c6b8b1873909e754110b5cebb25bbf573
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2020-12-22 13:26:51 +0100

    wordcloud from zotero titles (EPS_stics)

---
+++ R/wordcloud/wordcloud_title_zotero.R
@@ -0,0 +1,87 @@
+# source : http://www.sthda.com/french/wiki/text-mining-et-nuage-de-mots-avec-le-logiciel-r-5-etapes-simples-a-savoir
+# Installer
+# install.packages("tm")  # pour le text mining
+# install.packages("SnowballC") # pour le text stemming
+# install.packages("wordcloud") # générateur de word-cloud
+# install.packages("RColorBrewer") # Palettes de couleurs
+# Charger
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+library("DBI")
+library("RSQLite")
+library("dplyr")
+
+old_mar <- par("mar")
+
+bib_EPS <- SticsPubRTools:::get_references(library_name = "livre_rouge",
+                                                   collection_name = "EPS_stics")
+
+# Generating a data.frame
+refs <- data.frame(title=unlist(lapply(bib_EPS, function(x) x$title)),
+                         year=as.numeric(unlist(lapply(bib_EPS, function(x) x$year))),
+                   stringsAsFactors = FALSE) %>%
+  arrange(year)
+
+
+filter <- FALSE
+from <- min(refs$year) - 1
+to <- max(refs$year) + 1
+if (filter) refs %>% filter(year > from & year < to ) -> refs
+
+# add filter on years
+# TODO
+
+text <- unlist(lapply(refs[["title"]],
+                      function(x) strsplit(x = x, split = " ")))
+
+# Getting words from titles
+
+
+
+# Charger les données comme un corpus
+docs <- Corpus(VectorSource(text))
+
+# inspection du contenu
+#inspect(docs)
+
+
+# nettoyage du doc
+# Convertir le texte en minuscule
+docs <- tm_map(docs, content_transformer(tolower))
+# Supprimer les nombres
+docs <- tm_map(docs, removeNumbers)
+# Supprimer les mots vides anglais
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Supprimer votre propre liste de mots non désirés
+# TODO: faire une liste de mots à supprimer
+to_remove_fr <- c("sur", "des", "stics", "les", "dans" )
+to_remove_eng <- c("across", "due", "model", "new" )
+to_remove <- c(to_remove_fr, to_remove_eng )
+docs <- tm_map(docs, removeWords, to_remove)
+# Supprimer les ponctuations
+docs <- tm_map(docs, removePunctuation)
+# Supprimer les espaces vides supplémentaires
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+
+
+# Construire la matrice des mots
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+
+
+# genetation du nuage
+# remove margins
+par(mar = rep(0, 4))
+
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"), )
+
+par(mar=old_mar)
