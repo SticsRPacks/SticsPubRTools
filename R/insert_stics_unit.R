272912a539072c46469a72eb561667b639e0279d
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2021-03-25 11:17:05 +0100

    added: managing leading/trailing spaces and multiple spaces replacement, replacing multiplication dot with space fix: degree C formatting, code and ratio range mgmt, exponentiation including degree C formatting

--- R/insert_stics_unit.R
+++ R/insert_stics_unit.R
@@ -32,19 +32,35 @@ format_unit <- function(unit, escape = TRUE) {
 
   if (!any(not_code)) return(rep)
 
+  # removing leading and trailing blanks
+  rep[not_code] <- trimws(unit[not_code])
+
+  # International unit system: not dots for multiplication, space instead
+  rep[not_code] <- gsub(pattern = "\\.", x = rep[not_code], replacement = "\\ " )
+
+  # replacing multiple spaces
+  rep[not_code] <- gsub(' +','\\\\ ',rep[not_code])
+
+
   # replace SD
   # adimensional, non-dimensional, dimensionless
   # ND stands for non-dimensional
-  rep[not_code] <- gsub(pattern = "SD", x = unit[not_code], replacement = "ND" )
+  rep[not_code] <- gsub(pattern = "SD", x = rep[not_code], replacement = "ND" )
 
-  rep[not_code] <- gsub(pattern = "\\.", x = rep[not_code], replacement = " \\\\cdot " )
+
+  # rep[not_code] <- gsub(pattern = "\\.", x = rep[not_code], replacement = " \\\\cdot " )
+  ##rep[not_code] <- gsub(pattern = "\\ ", x = rep[not_code], replacement = "\\\\ " )
 
   # replace % -> \%
   rep[not_code] <- gsub(pattern = "\\%", x = rep[not_code], replacement = "\\\\%" )
 
   # replace degree_C -> $^{\circ}C$
-  rep[not_code] <- gsub(pattern = "degree_C", x = rep[not_code], replacement = "^{\\\\circ}C" )
-  rep[not_code] <- gsub(pattern = "([^a-zA-Z])degree_C", x = rep[not_code], replacement = " ^{\\\\circ}C" )
+  # rep[not_code] <- gsub(pattern = "degreeC", x = rep[not_code], replacement = "^{\\\\circ}C" )
+  # rep[not_code] <- gsub(pattern = "degree\\\\ C", x = rep[not_code], replacement = "^{\\\\circ}C" )
+  # rep[not_code] <- gsub(pattern = "degrees\\\\ C", x = rep[not_code], replacement = "^{\\\\circ}C" )
+  # rep[not_code] <- gsub(pattern = "degreesC", x = rep[not_code], replacement = "^{\\\\circ}C" )
+  # rep[not_code] <- gsub(pattern = "degree_C", x = rep[not_code], replacement = "^{\\\\circ}C" )
+  # rep[not_code] <- gsub(pattern = "([^a-zA-Z])degree_C", x = rep[not_code], replacement = " ^{\\\\circ}C" )
 
   # replace degree_d -> degree days
   rep[not_code] <- gsub(pattern = "degree_d", x = rep[not_code], replacement = "degree\\\\ days" )
@@ -65,6 +81,16 @@ format_unit <- function(unit, escape = TRUE) {
   #replace unit format
   rep[not_code] <-  format_expo_values(rep[not_code])
 
+  # Treating at last degree replacement, bc replacing them previously
+  # leading to exponents pbs.
+  rep[not_code] <- gsub(pattern = "degreeC", x = rep[not_code], replacement = "^{\\\\circ}C" )
+  rep[not_code] <- gsub(pattern = "degree\\\\ C", x = rep[not_code], replacement = "^{\\\\circ}C" )
+  rep[not_code] <- gsub(pattern = "degrees\\\\ C", x = rep[not_code], replacement = "^{\\\\circ}C" )
+  rep[not_code] <- gsub(pattern = "degreesC", x = rep[not_code], replacement = "^{\\\\circ}C" )
+  rep[not_code] <- gsub(pattern = "degree_C", x = rep[not_code], replacement = "^{\\\\circ}C" )
+  rep[not_code] <- gsub(pattern = "([^a-zA-Z])degree_C", x = rep[not_code], replacement = " ^{\\\\circ}C" )
+
+
 
   return(paste0("$",rep,"$"))
 
@@ -78,7 +104,10 @@ get_code_values <- function(code_str) {
     return(l)
   }
 
-  if (!grepl(pattern = "code",x = code_str)) return(NA)
+  code_found <- grepl(pattern = "code",x = code_str)
+  range_found <- grepl(pattern = "[0-9]-[0-9]",x = code_str)
+
+  if (!code_found && !range_found) return(NA)
 
   # add detecting "code" in code_str
   strs <- unique(strsplit(trimws(gsub("\\D+"," ",code_str)), split= " "))
@@ -86,7 +115,7 @@ get_code_values <- function(code_str) {
   if (!length(v)) {
     return(NA)
   } else {
-    c(min(v), max(v))
+    return(c(min(v), max(v), code_found))
   }
 
 }
@@ -98,27 +127,33 @@ format_code_values <- function(code_values) {
   #formats <- c("to", "from-to")
   #out_formats <- list("\\ to\\ ", c("from\\ ", "\\ to\\ "))
 
-
   format <- "\\ to\\ "
 
   if (is.list(code_values) & length(code_values) > 1) {
     l <- unlist(lapply(X = code_values, FUN = format_code_values))
     return(l)
-  } else {
-    code_values <- unlist(code_values)
+  # } else {
+  #   code_values <- unlist(code_values)
   }
 
   if (all(is.na(code_values))) return(code_values)
 
-  out <- paste0(paste0(as.character(code_values)), collapse = format)
-  paste0("code,\\ ", out)
+  code_found <- code_values[3]
+  code_values <- code_values[1:2]
 
+  out <- paste0(paste0(as.character(code_values)), collapse = format)
+  # adding code explicitly, if exists in initial unit
+  if (code_found) out <- paste0("code,\\ ", out)
+
+  out
 }
 
 format_expo_values <- function(unit) {
-  elts <- strsplit(unit,split=" ")[[1]]
-  #gsub(x = unit, pattern = "(.*)([-]?[0-9]{1,})(.*)", "\\1^{\\2}\\3" )
-  paste(gsub(x = elts, pattern = "(.*[^-])([-]?[0-9]{1})(.*)", "\\1^{\\2}\\3" ),
-        collapse = " ")
+  elts <- lapply(unit, function(x) strsplit(x,split=" ")[[1]] )
+  #elts <- strsplit(unit,split=" ")[[1]]
+  # paste(gsub(x = elts, pattern = "(.*[^-])([-]?[0-9]{1})(.*)", "\\1^{\\2}\\3" ),
+  #       collapse = " ")
+  unlist(lapply(elts, function(x) paste(gsub(x = x, pattern = "(.*[^-])([-]?[0-9]{1})(.*)", "\\1^{\\2}\\3" ),
+                                 collapse = " ") ))
 }
 
